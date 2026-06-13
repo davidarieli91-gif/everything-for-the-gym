daf85c2f49df28b1ebe0b939d11d37dbe8684d0f
davidarieli91-gif <david.arieli91@gmail.com>
2026-06-13 22:43:04 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
      с докачкой (растущий кэш)
    - Firebase/Google API: всегда сеть (не кэшируем)
    ============================================================ */
-const APP_CACHE = 'pg-app-v6';
+const APP_CACHE = 'pg-app-v10';
 const MEDIA_CACHE = 'pg-media-v1';
 
 const PRECACHE = [
